a7786dfbc63165c89d1b209cad78217e194365ac
Charles Butler <cdbutle@outlook.com>
2020-04-04 23:07:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,9 +13,6 @@ class Artist
     @songs << song
   end
 
-  def save
-    @@all << self #saves the artist name to the all array
-  end
 
   def self.all
     @@all
@@ -27,7 +24,6 @@ class Artist
       found_artist
     else
       new_artist = self.new(artist_name)
-      new_artist.save
       new_artist
     end
   end
